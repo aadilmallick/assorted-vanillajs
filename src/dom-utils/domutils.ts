f2e9326455ab0fc8c99c051813977af2128e746c
AADIL MALLICK <aadiltj.mallick@gmail.com>
2024-11-25 11:51:49 -0500

    Update domutils.ts

--- src/dom-utils/domutils.ts
+++ src/dom-utils/domutils.ts
@@ -4,7 +4,7 @@ export class DOM {
     return dom.body.firstElementChild as HTMLElement;
   }
   static $ = (selector: string): HTMLElement | null =>
-    document.querySelector(selector) as HTMLElement | null;
+    document.querySelector(selector);
   static $$ = (selector: string): NodeListOf<HTMLElement> =>
     document.querySelectorAll(selector);
 
@@ -26,15 +26,101 @@ export class DOM {
   }
 }
 
-export class AbortControllerManager {
-  private controller = new AbortController();
+export function html(strings: TemplateStringsArray, ...values: any[]) {
+  let str = "";
+  strings.forEach((string, i) => {
+    str += string + (values[i] || "");
+  });
+  return str;
+}
 
-  get signal() {
-    return this.controller.signal;
+export function css(strings: TemplateStringsArray, ...values: any[]) {
+  let str = "";
+  strings.forEach((string, i) => {
+    str += string + (values[i] || "");
+  });
+  return str;
+}
+
+export class CSSVariablesManager<T = Record<string, string>> {
+  constructor(private element: HTMLElement) {}
+
+  private formatName(name: string) {
+    if (name.startsWith("--")) {
+      return name;
+    }
+    return `--${name}`;
   }
 
-  abort() {
-    this.controller.abort();
+  set(name: keyof T, value: string) {
+    this.element.style.setProperty(this.formatName(name as string), value);
+  }
+
+  get(name: keyof T) {
+    return this.element.style.getPropertyValue(this.formatName(name as string));
+  }
+}
+
+export class LocalStorageBrowser<T extends Record<string, any>> {
+  constructor(private prefix: string = "") {}
+
+  private getKey(key: keyof T & string): string {
+    return this.prefix + key;
+  }
+
+  public set<K extends keyof T & string>(key: K, value: T[K]): void {
+    window.localStorage.setItem(this.getKey(key), JSON.stringify(value));
+  }
+
+  public get<K extends keyof T & string>(key: K): T[K] | null {
+    const item = window.localStorage.getItem(this.getKey(key));
+    return item ? JSON.parse(item) : null;
+  }
+
+  public removeItem(key: keyof T & string): void {
+    window.localStorage.removeItem(this.getKey(key));
+  }
+
+  public clear(): void {
+    window.localStorage.clear();
+  }
+}
+
+export class DateModel {
+  /**
+   *
+   * @param time the timestring like 20:35 to convert into a date
+   */
+  static convertTimeToDate(time: string) {
+    const date = new Date();
+    const [hours, minutes] = time.split(":");
+    date.setHours(parseInt(hours));
+    date.setMinutes(parseInt(minutes));
+    return date;
+  }
+
+  /**
+   *
+   * @param dateMillis the date in milliseconds to convert into a time string
+   */
+  static convertDateToTime(dateMillis: number) {
+    const date = new Date(dateMillis);
+    const hours = `${date.getHours()}`.padStart(2, "0");
+    const minutes = `${date.getMinutes()}`.padStart(2, "0");
+    return `${hours}:${minutes}`;
+  }
+
+  static militaryToStandardTime(militaryTime: string) {
+    const [hours, minutes] = militaryTime.split(":");
+    let hour = parseInt(hours, 10);
+    const ampm = hour >= 12 ? "PM" : "AM";
+
+    // Convert to 12-hour format
+    hour = hour % 12 || 12;
+
+    // Format the time string
+    const formattedTime = `${hour}:${minutes.padStart(2, "0")} ${ampm}`;
+    return formattedTime;
   }
 }
 
@@ -65,30 +151,5 @@ export class FullscreenModel {
 
   get isFullscreenEnabled() {
     return document.fullscreenEnabled;
-  }
-}
-
-export class LocalStorageBrowser<T extends Record<string, any>> {
-  constructor(private prefix: string = "") {}
-
-  private getKey(key: keyof T & string): string {
-    return this.prefix + key;
-  }
-
-  public set<K extends keyof T & string>(key: K, value: T[K]): void {
-    window.localStorage.setItem(this.getKey(key), JSON.stringify(value));
-  }
-
-  public get<K extends keyof T & string>(key: K): T[K] | null {
-    const item = window.localStorage.getItem(this.getKey(key));
-    return item ? JSON.parse(item) : null;
-  }
-
-  public removeItem(key: keyof T & string): void {
-    window.localStorage.removeItem(this.getKey(key));
-  }
-
-  public clear(): void {
-    window.localStorage.clear();
   }
 }
